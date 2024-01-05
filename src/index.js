085752eafe0efbdfbaae7575b9c015439d0fea50
Ritzsizl <ritzraha4u@gmail.com>
2024-01-05 22:07:01 +0530

    utils API

--- src/index.js
+++ src/index.js
@@ -10,6 +10,14 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    appendMutableCookies.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((err)=> {
+    console.log("MONGO db connection failed !!! ", err);
+})
 
 
 
